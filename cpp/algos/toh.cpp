8390399831cc7804502efdb1f021e8ebc9c0d568
Manish Lapasi <manish.lapasi@gmail.com>
2022-04-27 20:28:14 +0530

    toh

--- cpp/algos/toh.cpp
+++ cpp/algos/toh.cpp
@@ -4,21 +4,33 @@
 
 using namespace std;
 
-double num_moves(double n, int s1, int s2, int s3){
-
-    if(n==1){return 1;}
-    return pow(2,n-1) + num_moves(n-1, s3, s2, s1);
-
+vector<vector<int>> solve(int numToMove, int from, int to, int third, vector<vector<int>> arr){
+    if(numToMove==0){
+        return arr;
+    }
+    arr = solve(numToMove-1, from, third, to, arr);
+    arr[to].push_back(arr[from][arr[from].size()-1]);
+    arr[from].pop_back();
+    return solve(numToMove-1,third,to,from,arr);
 }
 
 int main(){
 
-    int s1 = 1, s2 = 2, s3 = 3;
-
-    double n;
-    cout<<"enter num of discs:";
-    cin>>n;
-    cout<<num_moves(n,s1,s2,s3);
-
+    std::vector<vector<int>> arr {{7,7,6,5,4,3,2,1},{},{}};
+    cout<<"start:"<<"\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<arr[i].size();j++){
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    vector<vector<int>> res = solve(arr[0].size(),0,2,1,arr);
+    cout<<"end:"<<"\n";
+    for(int i=0;i<3;i++){
+        for(int j=0;j<res[i].size();j++){
+            cout<<res[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
